d47bcf37c8d911eaee5af556046b25f750b61e23
Jagdish Sharma <jpsharma9456@gmail.com>
2021-03-15 01:17:28 +0530

    view files added

---
+++ optico/src/components/dashboard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <b-container class="container-main" fluid style="padding-left: 0; padding-right: 0">
+      <b-row>
+        <b-col cols="2" class="col">
+          <input
+            type="search"
+            id="form1"
+            class="form-control"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <hr />
+          <b-nav vertical class="w-25, nav-bar">
+            <b-nav-item>Home</b-nav-item>
+            <b-nav-item>Orders</b-nav-item>
+            <b-nav-item>Profile</b-nav-item>
+            <b-nav-item>Contact</b-nav-item>
+            <b-nav-item>Address</b-nav-item>
+          </b-nav>
+        </b-col>
+        <b-col cols="10" class="div-table">
+           <b-container fluid style="border-right: 1px solid black">
+            <b-row sm="12">
+                <b-col cols="12" class="header" style="display:inline-flex"> order Status
+                    <div style="right: 20px;position: absolute;">
+                    <b-dropdown size="lg" ropleft text="Drop-Left" variant="link" toggle-class="text-decoration-none" no-caret>
+                        <template #button-content>
+                        &#x1f50d;<span class="sr-only">Search</span>
+                        </template>
+                        <b-dropdown-item href="#">Action</b-dropdown-item>
+                        <b-dropdown-item href="#">Logout</b-dropdown-item>
+                    </b-dropdown>
+                    </div>    
+                </b-col>
+            </b-row>
+            </b-container>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+
+
+<style>
+.nav-bar {
+  font-family: emoji;
+  padding: 12px;
+  font-size: 23px;
+}
+.col {
+  height: 100vh;
+  color: #fff;
+    background-color: #122564;
+  box-shadow: 5px 5px 11px 0px #ccc;
+}
+.col ul li a {
+  color: #fff;
+}
+.form-control {
+  margin-top: 13px;
+}
+.header {
+  box-sizing: border-box;
+  background-color: #c1e0de;
+  height: 30%;
+  font-size: 35px;
+  font-family: fantasy;
+  color: #3781b1;
+  box-shadow: 5px 10px 8px #888888;
+}
+.container-main{
+    width:98%
+}
+</style>
---
+++ optico/src/components/login.vue
@@ -0,0 +1,126 @@
+<template>
+<section class="ftco-section">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-12 col-lg-10">
+        <div class="wrap d-md-flex">
+          <div class="img">
+              <img src="../assets/bg.png">
+          </div>
+          <div class="login-wrap p-md-5" style="padding-top:20%">
+            <div class="d-flex">
+              <div class="w-100">
+                <h3 class="mb-4">Login to Dashboard</h3>
+              </div>
+            </div>
+            <form action="#" class="signin-form">
+              <div class="form-group mb-3">
+                <label class="label" for="name">Username</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Username"
+                  required=""
+                />
+              </div>
+              <div class="form-group mb-3">
+                <label class="label" for="password">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  placeholder="Password"
+                  required=""
+                />
+              </div>
+              <div class="form-group d-md-flex">
+                <div class="w-100 text-left">
+                  <label class="checkbox-wrap checkbox-primary mb-0"
+                    >Remember Me
+                    <input type="checkbox" checked="" />
+                    <span class="checkmark"></span>
+                  </label>
+                </div>
+              </div>
+              <div class="form-group">
+                
+              </div>
+              <div class="form-group d-md-flex">
+                <div class="w-50 text-left">
+                 <button
+                  type="submit"
+                  class="form-control btn btn-primary rounded submit px-3"
+                >
+                  Sign In
+                </button>
+                </div>
+                <div class="w-50 text-md-right">
+                  <a href="#">Forgot Password</a>
+                </div>
+              </div>
+            </form>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+
+
+<script>
+export default {
+    
+}
+</script>
+<style>
+.ftco-section{
+    padding: 7em 0;
+}
+.justify-content-center {
+    -webkit-box-pack: center!important;
+    -ms-flex-pack: center!important;
+    justify-content: center!important;
+}
+.wrap {
+    width: 100%;
+    overflow: hidden;
+    background: #fff;
+    border-radius: 5px;
+    box-shadow: 0 10px 34px -15px rgb(0 0 0 / 24%);
+}
+@media (min-width: 768px){
+    .d-md-flex {
+        display: flex!important;
+    }
+    .p-md-5 {
+        padding: 10rem 2em 0em 2em !important;
+    }
+}
+
+.img, .login-wrap {
+    width: 50%;
+}
+.img > img{
+    width:100%;
+}
+.login-wrap {
+    position: relative;
+}
+.d-flex {
+    display: flex!important;
+}
+.form-group{
+    text-align: left;
+}
+.w-100{
+    width:100%;
+}
+.btn-primary {
+    color: #fff;
+    background-color: #122564;
+    border-color: #18276a;
+    height: 60px;
+}
+</style>
